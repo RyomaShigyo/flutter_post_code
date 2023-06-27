4b8cff6b7a3e3ae911b32a5d0b7fdc8a535c1160
RyomaShigyo <ryomax3781@gmail.com>
2023-06-27 20:17:27 +0900

    make FutureProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,53 +16,28 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends ConsumerWidget {
+  MyHomePage({super.key, required this.title});
   final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            TextField(),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ lib/provider.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:flutter_post_number/data/post_code.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
+
+StateProvider<String> postCodeProvider = StateProvider((ref) => '');
+
+FutureProvider<PostCode> apiProvider = FutureProvider((ref) async{
+  final postCode = ref.watch(postCodeProvider.notifier).state;
+  if(postCode.length != 7){
+    throw Exception('郵便番号は７桁を入力してください');
+  }
+
+  final upper = postCode.substring(0,3);//郵便番号の上３桁
+  final lower = postCode.substring(4);//郵便番号の下４桁
+
+  final apiUrl = 'https://madefor.github.io/postal-code-api/api/v1/$upper/$lower.json';
+  final apiUri = Uri.parse(apiUrl);
+
+  http.Response response = await http.get(apiUri);
+
+  if(response.statusCode != 200){
+    throw Exception('該当する郵便番号がありません');
+  }
+
+  var jsonData = json.decode(response.body);
+  return PostCode.fromJson(jsonData);
+});
